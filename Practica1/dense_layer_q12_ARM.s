8e2ee6c0863fa8f66bec4c10fe6e15f6f3ec7394
OMariscos <pablo.moave@gmail.com>
2026-09-15 00:39:38 +0200

    Añadido neuron y actualizado layer arm
    Falta testeo de ambos

--- Practica1/dense_layer_q12_ARM.s
+++ Practica1/dense_layer_q12_ARM.s
@@ -1,8 +1,8 @@
 AREA codigo, CODE, READONLY
-EXPORT  dense_layer_q12_ARM_C
-IMPORT  neuron_q12_C ;Esto cambiar a ARM y hacer neuron_q12_ARM? Por lo demas es igual
+EXPORT  dense_layer_q12_ARM
+IMPORT  neuron_q12_ARM
 	
-dense_layer_q12_ARM_C							
+dense_layer_q12_ARM							
 	PUSH {r4-r11, lr}		; Añadiremos en pila desde r4 a r7 los valores de los registros parámetro (r0-r3), el resto de parámetros que no caben entre r0 y r3 y las variables checksum y o 
 	mov r4, r0              ; input
     mov r5, r1          	; weights
@@ -19,18 +19,18 @@ buc
     mov r12, r12, LSL #1    ; r12 = o * input_size * 2  (bytes, los datos son de "half word")
     add r1,  r5, r12        ; r1 = weights_o  preparamos el peso para neuron
 	mov r12, r10, LSL #1    ; r12 = o*2 (bytes) — reutilizamos r12
-    ldrh r3, [r6, r12]      ; r3 = bias[o], para neuron
+    ldrsh r3, [r6, r12]      ; r3 = bias[o], para neuron
 	ldr r0, [sp, #48]		; r0=clamp_max
 	str r0, [sp, #-4]!		; clamp_max se almacena en sp, sp empuja a la direccion original de sp
 	ldr r0, [sp, #44]		; r0=clamp_min
 	str r0, [sp, #-4]!		; clamp_min se almacena en sp, sp empuja a la direccion original de sp
 	mov r0, r4				; r0=input (primer argumento de neuron)
 	mov r2, r8				; r2=input_size (tercer argumento de neuron)
-	bl neuron_q12_C
+	bl neuron_q12_ARM
 	add sp, sp, #8			;"Desapilamos" los clamps de neuron
 	mov r12, r10, LSL #1
 	strh r0, [r7, r12]		; output[o] = y
-	add r11, r11, LSL #5	; checksum=checksum*33u (añadir a memoria que hay otra solucion (mul r11, #33, r11)
+	add r11, r11, r11, LSL #5	; checksum=checksum*33u (añadir a memoria que hay otra solucion (mul r11, #33, r11)
 	and r0, r0, #0xFFFF		; (uint16_t)y, añadir a memoria, hace un and y mantiene las "direcciones bajas" dentro del registro dejando a 0 las direcciones altas, forzando a que esté a 16 bits (media palabra)
 	add r11, r11, r0
 	add r10, r10, #1
---
+++ Practica1/neuron_q12_ARM.s
@@ -0,0 +1,34 @@
+AREA codigo, CODE, READONLY
+EXPORT neuron_q12_ARM
+	
+neuron_q12_ARM
+	PUSH {r4-r11, lr}
+	mov r4, r0 				; input
+	mov r5, r1 				; weights
+	mov r6, r2 				; n
+	mov r7, r3 				; bias
+	mov r8, r7, LSL #12		; int32_t acc = ((int32_t)bias_q12) << Q_SHIFT, donde Q_SHIFT tiene un valor de 12, es decir, lo convertimos en un numero Q24
+	mov r9, #0				; i=0 del bucle
+buc
+	cmp r9, r6
+	beq finbuc
+	mov r12, r9, LSL #1 	; Alineamos a palabras de 2 bytes (ya que input y weitghts son uint16_t
+	ldrsh r0, [r4, r12]		; input[i]
+	ldrsh r1, [r5, r12]		; weights[i]
+	mla r8, r0, r1, r8		; acc += (int32_t)input[i] * (int32_t)weights[i], añadir en memoria, porque se puede disgregar en un mul y un add
+	add r9, r9, #1
+	b buc
+finbuc
+	mov r0, r8, ASR #12		; int32_t result_q12 = acc >> Q_SHIFT, guardamos el resultado de la acumulación y lo volvemos a convertir en un Q12
+	ldr r1, [sp, #36]		; r1 recibe el parametro clamp_min
+	ldr r2, [sp, #40]		; r2 recibe el parametro clamp_max
+							; No se llama a la función de saturación ya que la función de saturación es un static y no se pueden llamar desde otro fichero (static otorga encapsulamiento)
+	cmp     r0, r1
+    movlt   r0, r1          ; if (x < clamp_min) x = clamp_min
+    cmp     r0, r2
+    movgt   r0, r2          ; if (x > clamp_max) x = clamp_max
+	POP {r4-r11, pc}
+	END
+	
+	
+	
